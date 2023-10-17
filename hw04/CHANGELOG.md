ac37a555b356238500ded11ed061a7c667926401
tejchu <tstefanovs@jacobs-university.de>
2023-10-17 17:29:29 +0200

    Initial commit
    Signed-off-by: tejchu <tstefanovs@jacobs-university.de>

--- hw04/CHANGELOG.md
+++ hw04/CHANGELOG.md
@@ -1,4 +1,4 @@
-# Revision history for hw01
+# Revision history for hw04
 
 ## 0.1.0.0 -- YYYY-mm-dd
 
---
+++ hw04/app/Main.hs
@@ -0,0 +1,5 @@
+-- TODO: implement GADTs
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
