a002ff0a23caba55bbbf03e5f5151462a4e8d575
tejchu <tstefanovs@jacobs-university.de>
2023-11-03 14:10:38 +0100

    Changed Maybe to Either String Type
    Signed-off-by: tejchu <tstefanovs@jacobs-university.de>

--- stlc/src/TypeCheck.hs
+++ stlc/src/TypeCheck.hs
@@ -6,28 +6,34 @@ import Control.Monad (guard)
 
 type Env = M.Map String Type
 
-typeCheckEmpty :: Term String -> Maybe Type
+typeCheckEmpty :: Term String -> Either String Type
 typeCheckEmpty = typeCheck M.empty
 
-typeCheck :: Env -> Term String -> Maybe Type
+typeCheck :: Env -> Term String -> Either String Type
 typeCheck env (Var v) =
-  M.lookup v env
+  case M.lookup v env of
+    Just t -> Right t
+    Nothing -> Left $ "Variable '" ++ v ++ "' not in environment"
 typeCheck env (Abs x t b) = do
   let env' = M.insert x t env
   t1 <- typeCheck env' b
-  return $ Arrow t t1
+  Right (Arrow t t1)
 typeCheck env (App m n) = do
   t1 <- typeCheck env m
   t2 <- typeCheck env n
   case t1 of
-    Arrow t11 t12 | t2 == t11 -> return t12
-    _ -> Nothing
+    Arrow t11 t12 | t2 == t11 -> Right t12
+    _ -> Left "Type mismatch in application"
 typeCheck _ (BoolLit _) =
-  return Bool
+  Right Bool
 typeCheck env (If c t e) = do
   ct <- typeCheck env c
-  guard (ct == Bool)
-  tt <- typeCheck env t
-  et <- typeCheck env e
-  guard (tt == et)
-  return tt
+  case ct of
+    Bool -> do
+      tt <- typeCheck env t
+      et <- typeCheck env e
+      if tt == et
+        then Right tt
+        else Left "Branches in 'if' statement have different types"
+    _ -> Left "Condition in 'if' statement is not of type Bool"
+
