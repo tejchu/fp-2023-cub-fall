78423607a5f1f7f9e1ed4e6c1ac27db034f285e3
tejchu <tstefanovs@jacobs-university.de>
2023-11-03 14:29:08 +0100

    Added let bindings and arithmetics on ints to syntax
    Signed-off-by: tejchu <tstefanovs@jacobs-university.de>

--- stlc/src/Syntax.hs
+++ stlc/src/Syntax.hs
@@ -1,3 +1,4 @@
+
 {-# LANGUAGE FlexibleInstances #-}
 
 module Syntax where
@@ -12,6 +13,7 @@ data Type
   = TyVar String
   | Arrow Type Type
   | Bool
+  | Int -- add int type, I hope the name is fine
   deriving (Eq, Ord)
 
 instance Show Type where
@@ -19,6 +21,7 @@ instance Show Type where
   show (Arrow (Arrow t1 t2) t3) = printf  "(%s -> %s) -> %s" (show t1) (show t2) (show t3)
   show (Arrow t1 t2) = printf  "%s -> %s" (show t1) (show t2)
   show (Bool) = "Bool"
+  show (Int) = "Int" -- add show for ints
 
 data Term a
   = Var a
@@ -26,6 +29,12 @@ data Term a
   | App (Term a) (Term a)
   | BoolLit Bool
   | If (Term a) (Term a) (Term a)
+  | IntLit Int -- add int literal
+  | Add (Term a) (Term a) -- add arithmetic ops
+  | Sub (Term a) (Term a)
+  | Mul (Term a) (Term a)
+  | Div (Term a) (Term a)
+  | Let a (Term a) (Term a) -- add let binding
   deriving (Eq)
 
 instance Show (Term String) where
@@ -34,6 +43,11 @@ instance Show (Term String) where
   show (App t1 t2) = printf "(%s) (%s)" (show t1) (show t2)
   show (BoolLit b) = printf "%s" (show b)
   show (If c t e) = printf "if %s then %s else %s" (show c) (show t) (show e)
+  show (IntLit i) = printf "%s" (show i) -- add show for ints
+  show (Add t1 t2) = printf "(%s) + (%s)" (show t1) (show t2) -- add show for arithmetic ops
+  show (Sub t1 t2) = printf "(%s) - (%s)" (show t1) (show t2)
+  show (Mul t1 t2) = printf "(%s) * (%s)" (show t1) (show t2)
+  show (Div t1 t2) = printf "(%s) / (%s)" (show t1) (show t2)
 
 freeVars :: Ord a => Term a -> S.Set a
 freeVars (Var a) = S.singleton a
@@ -41,3 +55,9 @@ freeVars (Abs a _ t) = S.delete a (freeVars t)
 freeVars (App t1 t2) = S.union (freeVars t1) (freeVars t2)
 freeVars (BoolLit _) = S.empty
 freeVars (If c t e) = S.unions (freeVars <$> [c, t, e])
+freeVars (IntLit _) = S.empty -- add free vars for ints
+freeVars (Add t1 t2) = S.union (freeVars t1) (freeVars t2) -- I am not sure if I need these and whether I implemented them correctly
+freeVars (Sub t1 t2) = S.union (freeVars t1) (freeVars t2)
+freeVars (Mul t1 t2) = S.union (freeVars t1) (freeVars t2)
+freeVars (Div t1 t2) = S.union (freeVars t1) (freeVars t2)
+freeVars (Let x t1 t2) = S.union (freeVars t1) (S.delete x (freeVars t2)) -- for let binding
