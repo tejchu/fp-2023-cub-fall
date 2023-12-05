3896d5406e28e0736e486d5ee4992b163b653b0a
tejchu <tstefanovs@jacobs-university.de>
2023-12-05 22:29:33 +0100

    added tests
    Signed-off-by: tejchu <tstefanovs@jacobs-university.de>

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,88 @@
-
-module Main where
-import Test.QuickCheck
+import Test.Hspec
 import RegExpLibrary
+import Test.QuickCheck
+
+
+main :: IO ()
+main = do
+  -- Property-Based Tests
+  putStrLn "Running Property-Based Tests..."
+  quickCheck prop_ReverseDoubleNegation
+  quickCheck prop_MatchReversed
+  hspec $ do
+    describe "nullable" $ do
+      it "returns False for Empty" $
+        nullable Empty `shouldBe` False
+
+      it "returns True for Epsilon" $
+        nullable Epsilon `shouldBe` True
+
+      it "returns False for Literal" $
+        nullable (Literal 'a') `shouldBe` False
+
+      it "returns True for Concatenation of nullable regexps" $
+        nullable (Concat Epsilon Epsilon) `shouldBe` True
+
+      it "returns False for Concatenation of non-nullable regexps" $
+        nullable (Concat Epsilon (Literal 'a')) `shouldBe` False
+
+      it "returns True for Union of nullable regexps" $
+        nullable (Union Epsilon Empty) `shouldBe` True
+
+      it "returns False for Union of non-nullable regexps" $
+        nullable (Union (Literal 'a') (Literal 'b')) `shouldBe` False
+
+      it "returns True for Star of any regexp" $
+        nullable (Star (Literal 'a')) `shouldBe` True
+
+    describe "deriv" $ do
+      it "returns Empty for Empty" $
+        deriv 'a' Empty `shouldBe` Empty
+
+      it "returns Empty for Epsilon" $
+        deriv 'a' Epsilon `shouldBe` Empty
+
+      it "returns Epsilon for matching Literal" $
+        deriv 'a' (Literal 'a') `shouldBe` Epsilon
+
+      it "returns Empty for non-matching Literal" $
+        deriv 'a' (Literal 'b') `shouldBe` Empty
+
+      it "returns Union of derivs for Union" $
+        deriv 'a' (Union (Literal 'a') (Literal 'b')) `shouldBe` Union Epsilon Empty
+
+      it "returns Concatenation of deriv and Star for Star" $
+        deriv 'a' (Star (Literal 'a')) `shouldBe` Concat Epsilon (Star (Literal 'a'))
+
+    describe "match" $ do
+      it "returns True for matching regexp and string" $
+        match (Literal 'a') "a" `shouldBe` True
+
+      it "returns False for non-matching regexp and string" $
+        match (Literal 'a') "b" `shouldBe` False
+
+      it "returns True for matching regexp and empty string" $
+        match Epsilon "" `shouldBe` True
+
+      it "returns True for matching regexp and string with nullable parts" $
+        match (Concat Epsilon (Literal 'a')) "a" `shouldBe` True
+
+      it "returns False for non-matching regexp and string with nullable parts" $
+        match (Concat Epsilon (Literal 'a')) "b" `shouldBe` False
+
+      it "returns True for matching regexp and string with union of nullable parts" $
+        match (Union Epsilon Empty) "" `shouldBe` True
+
+      it "returns False for non-matching regexp and string with union of non-nullable parts" $
+        match (Union (Literal 'a') (Literal 'b')) "" `shouldBe` False
+
+      it "returns True for matching regexp and string with star of any regexp" $
+        match (Star (Literal 'a')) "aaa" `shouldBe` True
+
+      it "returns False for non-matching regexp and string with star of any regexp" $
+        match (Star (Literal 'a')) "aab" `shouldBe` False
+
+
 
 instance Arbitrary RegExp where
   arbitrary = sized arbRegExp
@@ -29,9 +110,3 @@ prop_MatchReversed :: RegExp -> String -> Bool
 prop_MatchReversed r s = match r (reverse s) == match (reverseRegExp r) s
 
 
-
-main :: IO ()
-main = do
-  putStrLn "Running Property-Based Tests..."
-  quickCheck prop_ReverseDoubleNegation
-  quickCheck prop_MatchReversed
